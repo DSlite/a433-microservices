cdc5f51ee1814a96e679b0a1b2c0438235585b40
Dindra Setyadharma <setyadharma.dindra@gmail.com>
2023-07-30 22:45:04 +0700

    Uploading submission

--- build_push_image_karsajobs-ui.sh
+++ build_push_image_karsajobs-ui.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Building Karsajobs-UI image
 docker build -t ghcr.io/dslite/karsajobs-ui:latest .
 
+# Login to ghcr.io
 echo $TOKEN | docker login ghcr.io --username dslite --password-stdin
 
+# Pushing Karsajob-UI image
 docker push ghcr.io/dslite/karsajobs-ui:latest
